640f182aeb119755d053a662a1183fb756a12469
yihua.huang <code4crafter@gmail.com>
2014-01-26 18:47:31 +0800

    fork

--- src/hello-fork.c
+++ src/hello-fork.c
@@ -3,12 +3,13 @@
 int main(int argc, const char* argv[]) {
   pid_t pid;
   printf("Hello, World!%d\n",pid);
-  sleep(2);
-  pid = fork();
-  if (pid == 0){
-    printf("I am child");
-  } else {
-    printf("I am parent, my child is %d",pid);
+  for (int i=0;i<2;i++){
+      pid = fork();
+      if (pid == 0){
+        printf("I am child");
+      } else {
+        printf("I am parent, my child is %d",pid);
+      }
   }
   return 0;
 }
